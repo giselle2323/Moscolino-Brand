033b9402681bc91291d7fb8078c08320fd83436c
Amina <yakubuaminat94@gmail.com>
2021-01-10 01:07:56 +0100

    bug: unused state for navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React  from "react";
 export const Nav = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     
